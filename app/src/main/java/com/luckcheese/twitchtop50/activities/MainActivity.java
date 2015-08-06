95ff4e1c777dd1636d702349ac19e4edb1484a2d
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2015-08-05 22:48:36 -0300

    Made MainActivity generic so it can show result as ListView or GridView, and added a toggle view type button on action bar

--- app/src/main/java/com/luckcheese/twitchtop50/activities/MainActivity.java
+++ app/src/main/java/com/luckcheese/twitchtop50/activities/MainActivity.java
@@ -2,44 +2,106 @@ package com.luckcheese.twitchtop50.activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.luckcheese.twitchtop50.R;
 
 public class MainActivity extends Activity {
 
+    public static final String INTENT_VIEW_TYPE = "com.luckcheese.twitchtop50.INTENT_VIEW_TYPE";
+
+    private ViewTypes viewType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        setContentView(R.layout.activity_main_list);
-//        ListView listView = (ListView) findViewById(R.id.listView);
-//        listView.setAdapter(new ListAdapter(this));
+        if (getIntent().hasExtra(INTENT_VIEW_TYPE)) {
+            viewType = (ViewTypes) getIntent().getSerializableExtra(INTENT_VIEW_TYPE);
+        } else {
+            viewType = ViewTypes.ListView;
+        }
 
-        setContentView(R.layout.activity_main_grid);
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-        gridView.setAdapter(new ListAdapter(this));
+        int adapterResource = 0;
+        switch (viewType) {
+            case GridView:
+                setContentView(R.layout.activity_main_grid);
+                adapterResource = R.layout.grid_item;
+                break;
+
+            case ListView:
+                setContentView(R.layout.activity_main_list);
+                adapterResource = R.layout.list_item;
+                break;
+        }
+
+        AbsListView listView = (AbsListView) findViewById(R.id.listView);
+        listView.setAdapter(new ListAdapter(this, adapterResource));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        switch (viewType) {
+            case GridView:
+                inflater.inflate(R.menu.activity_main_grid, menu);
+                break;
+
+            case ListView:
+                inflater.inflate(R.menu.activity_main_list, menu);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        if (item.getItemId() != R.id.toggleView) return false;
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        switch (viewType) {
+            case GridView:
+                intent.putExtra(INTENT_VIEW_TYPE, ViewTypes.ListView);
+                break;
+
+            case ListView:
+                intent.putExtra(INTENT_VIEW_TYPE, ViewTypes.GridView);
+                break;
+        }
+
+        startActivity(intent);
+        return true;
     }
 
     // ----- Related classes --------------------------------------------------
 
+    public static enum ViewTypes {
+        ListView, GridView
+    }
+
     private static final class ListAdapter extends ArrayAdapter<Void> {
 
         private LayoutInflater inflater;
+        private int resource;
 
-        public ListAdapter(Context context) {
-//            super(context, R.layout.list_item);
-            super(context, R.layout.grid_item);
+        public ListAdapter(Context context, int resource) {
+            super(context, resource);
 
             inflater = LayoutInflater.from(context);
+            this.resource = resource;
         }
 
         @Override
@@ -50,8 +112,7 @@ public class MainActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-//                convertView = inflater.inflate(R.layout.list_item, parent, false);
-                convertView = inflater.inflate(R.layout.grid_item, parent, false);
+                convertView = inflater.inflate(resource, parent, false);
             }
 
             ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
