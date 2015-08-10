3653d91b5941ebe3cd93cdfd3af7fc9a45a67c59
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2015-08-09 22:35:56 -0300

    Improved TwitchReturnModel so it can fetch the top 50 games

--- app/src/main/java/com/luckcheese/twitchtop50/models/TwitchReturnModel.java
+++ app/src/main/java/com/luckcheese/twitchtop50/models/TwitchReturnModel.java
@@ -1,5 +1,11 @@
 package com.luckcheese.twitchtop50.models;
 
+import android.content.Context;
+
+import com.android.volley.toolbox.JsonRequest;
+import com.luckcheese.twitchtop50.TwitchTop50App;
+import com.luckcheese.twitchtop50.requests.TwitchRequest;
+
 import org.json.JSONObject;
 
 import java.io.Serializable;
@@ -13,6 +19,14 @@ public class TwitchReturnModel extends JSONObject implements Serializable {
 
     public List<Game.Entry> getTop() {
         return top;
+    }
+
+    // ----- Static methods ---------------------------------------------------
+
+    public static void fetch(Context context) {
+        String firstPageUrl = "https://api.twitch.tv/kraken/games/top?limit=50";
+        JsonRequest stringRequest = new TwitchRequest(context, firstPageUrl);
+        TwitchTop50App.getQueue().add(stringRequest);
     }
 
     // ----- Related classes --------------------------------------------------
